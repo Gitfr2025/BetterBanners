a251fbcbf8426afd32be7ccb807aa2cdc625d3ed
Max <89785011+Gitfr2025@users.noreply.github.com>
2022-09-12 20:14:09 +0200

    Update to gradle and toml

--- src/main/java/com/example/bb/BetterBanners.java
+++ src/main/java/com/example/bb/BetterBanners.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.example.bb;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,13 +18,13 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(BetterBanners.MOD_ID)
+public class BetterBanners
 {
-    // Directly reference a slf4j logger
+    public static final String MOD_ID = "bb";
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod()
+    public BetterBanners()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
